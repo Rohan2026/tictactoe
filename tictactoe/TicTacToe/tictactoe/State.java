41b8086dad17e07243618a03adebc6e5796cbf93
Rohan2026 <143441935+Rohan2026@users.noreply.github.com>
2024-01-09 11:39:17 -0800

    Add files via upload

--- tictactoe/TicTacToe/tictactoe/State.java
+++ tictactoe/TicTacToe/tictactoe/State.java
@@ -10,5 +10,21 @@ public class State
     private String xName = "";
     private String oName = "";
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+    
+    public int getwhoseMove() {
+        return this.whoseMove;
+    }
+    
+    public void setwhoseMove() {
+        this.whoseMove = whoseMove;
+    }
+    
+    //public boolean isWinner() {
+        //e
+   // }
+    
+    //public boolean isTie() {
+        //e
+    //}
 }
 
