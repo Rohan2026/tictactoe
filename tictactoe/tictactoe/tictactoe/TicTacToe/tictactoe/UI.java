566c612684ac47c6e13d7ed3340e86647e740911
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2024-01-22 11:46:55 -0800

    1/22

--- tictactoe/tictactoe/tictactoe/TicTacToe/tictactoe/UI.java
+++ tictactoe/tictactoe/tictactoe/TicTacToe/tictactoe/UI.java
@@ -14,7 +14,13 @@ public class UI
     }
     // Utility methods
     public String getXOrO(int whoseMove) {
-        return (whoseMove == -1) ? "X" : "O";
+        if (whoseMove == -1) {
+            return "X";
+        } else if (whoseMove == 1) {
+            return "O";
+        } else {
+            return " ";
+        }
     }
 
     public String getPlayerName(int whoseMove, String XName, String OName) {
@@ -28,7 +34,7 @@ public class UI
     }
 
     // Prompt for input methods
-    public String promptForName(int player) {
+    public String promptForName(String player) {
         System.out.printf(Constants.GET_PLAYER_NAME, player);
         return scanner.next();
     }
@@ -74,6 +80,7 @@ public class UI
         System.out.println(Constants.DIVIDER_STRING);
         for (int row = 0; row < Constants.BOARD_SIZE; row++) {
             System.out.printf(Constants.BOARD_STRING, getXOrO(state.getBoardCell(row, 0)), getXOrO(state.getBoardCell(row, 1)), getXOrO(state.getBoardCell(row, 2)));
+            System.out.println();
             System.out.println(Constants.DIVIDER_STRING);
         }
     }
@@ -92,6 +99,7 @@ public class UI
 
     public void printWinner(State state) {
         System.out.printf(Constants.WINNER, getXOrO(state.getwhoseMove()), getPlayerName(state.getwhoseMove(), state.getXName(), state.getOName()));
+        System.out.println();
     }
 
     public void printTieGame() {
